3ab3aaec49ee9d7fb6859992b6fcfb655e887010
oscar <oscar.k.joh@gmail.com>
2023-11-05 20:57:29 +0100

    works, but needs some fixes

---
+++ script.js
@@ -0,0 +1,22 @@
+﻿
+document.getElementById('id_button_load').onclick = () => {
+  chrome.storage.sync.get(['cookie'], function(items) {
+    console.log('Settings retrieved', items);
+    let value = document.getElementById('id_cookiestring').value = items
+  });
+}
+
+document.getElementById('id_button_save').onclick = () => {
+  let value = document.getElementById('id_cookiestring').value;
+  cookie = document.cookie = value
+  chrome.storage.sync.set({'cookie': cookie}, function() {
+    console.log('Settings saved', );
+  });
+}
+
+document.getElementById('id_button_set').onclick = () => {
+  let value = document.getElementById('id_cookiestring').value;
+  cookie = document.cookie = value
+  chrome.cookies.set(cookie)
+}
+
